48e9817564db0d7040e8b2c7d4d46eb1037d369c
yuanbo <yuan.bo@live.com>
2018-10-04 02:46:28 +0800

    add zen before wechat

--- bot.js
+++ bot.js
@@ -4,15 +4,15 @@ const bot = new Telegraf(process.env.TOKEN, { username: process.env.BOT_USERNAME
 bot.command('start', (ctx) => ctx.reply(`
 你好，我是ZEN中文机器人，我可以告诉你一些ZEN的情况，发送"/zencommands"来看看我知道什么。
 `));
-bot.command('wechat', (ctx) => ctx.replyWithHTML('<b>官方微信</b>：ZenCashOfficial'));
+bot.command('zenwechat', (ctx) => ctx.replyWithHTML('<b>ZEN官方微信</b>：ZenCashOfficial'));
 bot.command('website', (ctx) => ctx.reply('https://horizen.global/zh/'));
 
 
 //--------------------------------------------
 bot.command('zencommands', (ctx) => ctx.replyWithHTML(`
 /zencommands - 全部机器人指令
-/wechat - 官方微信
-/website - 官网
+/zenwechat - ZEN官方微信
+/website - ZEN官网
 
 更多功能我正在努力学习中,敬请期待。
 `));
