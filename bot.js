c5f405dcc41056a5b62301239658b5cd6a01a293
yuanbo <yuan.bo@live.com>
2018-10-04 02:43:21 +0800

    ZEN instead of Zen

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Telegraf = require('telegraf');
 
 const bot = new Telegraf(process.env.TOKEN, { username: process.env.BOT_USERNAME });
 bot.command('start', (ctx) => ctx.reply(`
-你好，我是Zen中文机器人，我可以告诉你一些Zen的情况，发送"/zencommands"来看看我知道什么。
+你好，我是ZEN中文机器人，我可以告诉你一些ZEN的情况，发送"/zencommands"来看看我知道什么。
 `));
 bot.command('wechat', (ctx) => ctx.replyWithHTML('<b>官方微信</b>：ZenCashOfficial'));
 bot.command('website', (ctx) => ctx.reply('https://horizen.global/zh/'));
